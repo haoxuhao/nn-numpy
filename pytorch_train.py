a5cc9187210757fa94219f7b7abc31619894be4d
haoxuhao <haoxu96@qq.com>
2020-03-27 17:48:55 +0800

    train on mnist dataset

--- pytorch_train.py
+++ pytorch_train.py
@@ -2,30 +2,54 @@
 
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.datasets import load_iris 
+from sklearn.datasets import load_iris, load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
-
 import torch
-
-# print(torch.optim.__file__)
 import torch.nn as nn
 import torch.optim as optim
 import sys
+import random
 
 if __name__ == "__main__":
     random_seed=666
     np.random.seed(random_seed)
     torch.manual_seed(random_seed)
 
-    # prepare the dataset
-    iris = load_iris()
-    X = iris.data
-    target = iris.target
+    # # iris dataset
+    # iris = load_iris()
+    # X = iris.data
+    # target = iris.target
+    # num_classes = 3
+    # num_features = 4
+    # # build a neural network
+    # net = nn.Sequential(
+    #     nn.Linear(num_features, 20),
+    #     nn.ReLU(),
+    #     nn.Linear(20, 15),
+    #     nn.ReLU(),
+    #     nn.Linear(15, num_classes),
+    # )
 
-    num_classes = 3
-    num_features = 4
+    # mnist dataset
+    mnist = load_digits()
+    X = mnist.data
+    target = mnist.target
+    num_classes = 10
+    num_features = 64
+    # build a neural network
+    net = nn.Sequential(
+        nn.Linear(num_features, 256),
+        nn.ReLU(),
+        nn.Linear(256, 128),
+        nn.ReLU(),
+        nn.Linear(128, 64),
+        nn.ReLU(),
+        nn.Linear(64, 32),
+        nn.ReLU(),
+        nn.Linear(32, num_classes),
+    )
+
     X_normalized = StandardScaler().fit_transform(X)
     
     X_train, X_test, y_train, y_test = train_test_split(X_normalized, \
@@ -34,14 +58,6 @@ if __name__ == "__main__":
     x_train, x_test = torch.tensor(X_train, dtype=torch.float), torch.tensor(X_test, dtype=torch.float)
     y_train, y_test = torch.tensor(y_train, dtype=torch.long), torch.tensor(y_test, dtype=torch.long)
     
-    net = nn.Sequential(
-        nn.Linear(4, 20),
-        nn.ReLU(),
-        nn.Linear(20, 15),
-        nn.ReLU(),
-        nn.Linear(15, 3),
-    )
-
     # evaluate before train
     y_pred = net(x_test)
     y_pred = torch.argmax(y_pred, dim=1)
@@ -49,18 +65,26 @@ if __name__ == "__main__":
 
     # loss
     criterion = nn.CrossEntropyLoss()
-
-    base_lr = 0.01
-    # optimizer = optim.SGD(net.parameters(), lr=base_lr)
-    batch = 16
-    epochs = 150
-
-    optimizer = optim.Adam(net.parameters(), lr=0.05)
     
+    base_lr = 0.1
+    optimizer = optim.SGD(net.parameters(), lr=base_lr)
+    batch = 64
+    epochs = 30
+
+    # optimizer = optim.Adam(net.parameters(), lr=0.05)
+
+    data_indexs = [a for a in range(X_train.shape[0])]
+
     for epoch in range(epochs):
-        for i in range(0, X_train.shape[0], batch):
-            x = x_train[i:i+batch, :].reshape(-1, num_features)
-            y_true = y_train[i:i+batch]
+        random.shuffle(data_indexs) #SGD shuffle training data indexs every epoch
+        for i in range(0, len(data_indexs), batch):
+            random_batch = data_indexs[i:i+batch]
+
+            x = x_train[random_batch, :].reshape(-1, num_features)
+            y_true = y_train[random_batch]
+
+            # x = x_train[i:i+batch, :].reshape(-1, num_features)
+            # y_true = y_train[i:i+batch]
 
             optimizer.zero_grad()
             y_pred = net(x)
--- train.py
+++ train.py
@@ -3,74 +3,96 @@
 from feedforward_nn import NN, CrossEntropyLoss
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.datasets import load_iris 
+from sklearn.datasets import load_iris, load_digits
 from sklearn.model_selection import train_test_split
 from functions import softmax
 from sklearn.metrics import accuracy_score
 import math
+from tqdm import tqdm
+import random
 
-
-if __name__ == "__main__":
+def train():
     random_seed=666
     np.random.seed(random_seed)
 
-    # prepare the dataset
-    iris = load_iris()
-    X = iris.data
-    target = iris.target
-
-    num_classes = 3
-    num_features = 4
-    X_normalized = StandardScaler().fit_transform(X)
-    
-    X_train, X_test, y_train, y_test = train_test_split(X_normalized, \
-        target.reshape(-1), test_size=0.30, random_state=random_seed)
-
-    y_train_onehot = np.eye(num_classes)[y_train.reshape(-1)]
-
+    # mnist dataset
+    mnist = load_digits()
+    X = mnist.data
+    target = mnist.target
+    num_classes = 10
+    num_features = 64
     # build a neural network
-    net = NN(layers=[num_features, 20, 15, num_classes], activations=["relu", "relu", "linear"])
+    net = NN(layers=[num_features, 256, 128, 64, 32, num_classes], activations=["relu", "relu", "relu", "relu", "linear"])
+
+    # # iris dataset
+    # iris = load_iris()
+    # X = iris.data
+    # target = iris.target
+    # num_classes = 3
+    # num_features = 4
+    # # build a neural network
+    # net = NN(layers=[num_features, 20, 10, num_classes], activations=["relu", "relu", "linear"])
     
     # loss
     criteria = CrossEntropyLoss()
 
     # hyperparameters
-    base_lr = 0.01
-    batch = 16
-    epochs = 150
+    base_lr = 0.1
+    batch = 64
+    epochs = 30
+    
+
+    X_normalized = StandardScaler().fit_transform(X)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X_normalized, \
+        target.reshape(-1), test_size=0.30, random_state=random_seed)
+
+    print("train size: %d; test size: %d"%(X_train.shape[0], X_test.shape[0]))
+
+    y_train_onehot = np.eye(num_classes)[y_train.reshape(-1)]
 
     #eval before training
-    test_output = net.forward(X_test)
-    y_pred = np.argmax(test_output, axis=1)
-    acc_before_train = accuracy_score(y_test, y_pred)
-    
-    #clear cache
-    net.no_grad()
+    def eval(net, eval_data, eval_label):
+        test_output = net.forward(eval_data)
+        y_pred = np.argmax(test_output, axis=1)
+        acc = accuracy_score(eval_label, y_pred)
+        #clear cache
+        net.no_grad()
+        return acc
 
+    acc_before_train = eval(net, X_test, y_test)
+    acc = acc_before_train
+
+    data_indexs = [a for a in range(X_train.shape[0])]
+    lr = base_lr
+    loss = 0
     for epoch in range(epochs):
-        for i in range(0, X_train.shape[0], batch):
-            x = X_train[i:i+batch, :].reshape(-1, num_features)
-            y_true = y_train_onehot[i:i+batch, :].reshape(-1, num_classes)
+        random.shuffle(data_indexs)
+        for i in tqdm(range(0, len(data_indexs), batch), desc="epoch %d; train loss %.4f; test acc %.3f; lr %.5f"%(epoch+1, loss, acc, lr)):
+            if i==0: loss=0
+            indexs = data_indexs[i:i+batch]
+            x = X_train[indexs, :].reshape(-1, num_features)
+            y_true = y_train_onehot[indexs, :].reshape(-1, num_classes)
 
             y = net.forward(x)
-            loss = criteria(y, y_true)
+            loss += criteria(y, y_true)
             net.backward(criteria.grad)
 
-            lr = base_lr * 1 #math.pow(0.1, epoch//100)
+            lr = base_lr
             
             net.update(lr)
 
-            print("epoch %d, batch %d, loss %.6f"%(epoch, i, loss))
-            # break
+            # print("epoch %d, batch %d, loss %.6f"%(epoch, i, loss), flush=True)
 
-
-    #eval after training
-    test_output = net.forward(X_test)
-    y_pred = np.argmax(test_output, axis=1)
+        acc = eval(net, X_test, y_test)
+        
 
     print("accuracy before train: %.3f"%acc_before_train)
-    print("accuracy after train: %.3f"%accuracy_score(y_test, y_pred))
-    
+    print("accuracy after train: %.3f"%acc)
+
+
+if __name__ == "__main__":
+    train()
 
 
     
